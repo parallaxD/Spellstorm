c141b7db77b1cdda7ba2348b9c65de59dae3ed45
Кристина <126091715+KristinaVavilina@users.noreply.github.com>
2025-11-17 16:42:07 +0500

    add pausePanel and update UIManager

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -6,37 +6,69 @@ public class UIManager : MonoBehaviour
     public GameObject mainMenuPanel;
     public GameObject settingsPanel;
     public GameObject gamePanel;
+    public GameObject pausePanel;
 
     private GameObject[] panels;
+    private GameObject lastPanel;
+
+    public PauseManager pauseManager;
 
     void Awake()
     {
+
         panels = new GameObject[]
         {
             mainMenuPanel,
             gamePanel,
-            settingsPanel
+            settingsPanel,
+            pausePanel
         };
 
         ShowMainMenu();
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ShowPause();
+        }
+    }
+
     public void ShowMainMenu()
     {
-        SetAllPanelsInactive();
-        mainMenuPanel.SetActive(true);
+        pauseManager.PauseGame();
+        ShowPanel(mainMenuPanel);
     }
 
     public void ShowGame()
     {
-        SetAllPanelsInactive();
-        gamePanel.SetActive(true);
+        pauseManager.ResumeGame();
+        ShowPanel(gamePanel);
+    }
+
+    public void ShowPause()
+    {
+        pauseManager.PauseGame();
+        ShowPanel(pausePanel);
     }
 
     public void ShowSettings()
     {
+        ShowPanel(settingsPanel);
+    }
+
+    public void CloseSettings()
+    {
+        ShowPanel(lastPanel);
+    }
+
+    private void ShowPanel(GameObject panel)
+    {
         SetAllPanelsInactive();
-        settingsPanel.SetActive(true);
+        panel.SetActive(true);
+
+        if (panel != settingsPanel) lastPanel = panel;
     }
 
     private void SetAllPanelsInactive()
