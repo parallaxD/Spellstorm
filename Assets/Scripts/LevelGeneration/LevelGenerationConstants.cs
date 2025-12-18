d47262a9625f35e329571920eaf346b7a6c0d1dc
Кристина <126091715+KristinaVavilina@users.noreply.github.com>
2025-12-18 16:50:25 +0500

    add simple corridorTilesGenerator

---
+++ Assets/Scripts/LevelGeneration/LevelGenerationConstants.cs
@@ -0,0 +1,8 @@
+﻿public class LevelGenerationConstants
+{
+    public static float backgroundSizeOffset = 5f;
+
+    public static float roomCollidersSizeOffset = 3f;
+
+    public static float decorationSizeOffset = 3f;
+}
--- Assets/Scripts/LevelGeneration/TilesGeneration/GenerationAlgorithms/Colliders/ColliderTilesGenerator.cs
+++ Assets/Scripts/LevelGeneration/TilesGeneration/GenerationAlgorithms/Colliders/ColliderTilesGenerator.cs
@@ -1,21 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class ColliderTilesGenerator : SimpleRandomWalkDungeonGenerator
 {
-    [SerializeField] private float sizeOffset = 3f;
+    [SerializeField] private float sizeOffset = LevelGenerationConstants.roomCollidersSizeOffset;
+
     public static List<HashSet<Vector2>> roomPositionsList;
     public static List<HashSet<Vector2>> corridorPositionsList;
 
     public void GenerateColliderTiles()
     {
-        var positions = GetColliderTilesPositions();
-        pathVizualizer.PaintColliderTiles(positions);
+        var corridorColliders = GetCorridorColliderTilesPositions();
+        var roomColliders = GetRoomColliderTilesPositions();
+
+        pathVizualizer.PaintColliderTiles(roomColliders);
+        pathVizualizer.PaintColliderTiles(corridorColliders);
     }
 
-    private HashSet<Vector2> GetColliderTilesPositions()
+    private HashSet<Vector2> GetRoomColliderTilesPositions()
     {
         var result = new HashSet<Vector2>();
 
@@ -26,9 +29,9 @@ public class ColliderTilesGenerator : SimpleRandomWalkDungeonGenerator
             var yStart = room.Min(p => p.y) - sizeOffset;
             var yEnd = room.Max(p => p.y) + sizeOffset;
 
-            for (float x = xStart; x < xEnd; x += 0.5f)
+            for (var x = xStart; x < xEnd; x += 0.5f)
             {
-                for (float y = yStart; y < yEnd; y += 0.5f)
+                for (var y = yStart; y < yEnd; y += 0.5f)
                 {
                     var position = new Vector2(x, y);
 
@@ -41,6 +44,35 @@ public class ColliderTilesGenerator : SimpleRandomWalkDungeonGenerator
         }
 
         return result;
+    }
+
+    private HashSet<Vector2> GetCorridorColliderTilesPositions()
+    {
+        var result = new HashSet<Vector2>();
+
+        foreach (var corridor in corridorPositionsList)
+        {
+            var xStart = corridor.Min(p => p.x) - sizeOffset;
+            var xEnd = corridor.Max(p => p.x) + sizeOffset;
+            var yStart = corridor.Min(p => p.y) - sizeOffset;
+            var yEnd = corridor.Max(p => p.y) + sizeOffset;
+
+            for (var x = xStart; x < xEnd; x += 0.5f)
+            {
+                for (var y = yStart; y < yEnd; y += 0.5f)
+                {
+                    var position = new Vector2(x, y);
+
+                    if (x == xStart || x == xEnd - 1 || y == yStart || y == yEnd - 1)
+                    {
+                        result.Add(position);
+                    }
+                }
+            }
+        }
+
+        return result;
+        // Стереть все, которые входят в область друг друга
     }
 
     private bool IsPointOutsideAllCorridors(Vector2 position)
